5631339a59099c98680b94b60f714411de86103b
sky2464 <31672446+sky2464@users.noreply.github.com>
2018-02-27 22:06:28 -0500

    Update

--- Queen-N/src/n-n-queens.cpp
+++ Queen-N/src/n-n-queens.cpp
@@ -1,100 +1,155 @@
 //============================================================================
-// Name        : n-n-queens.cpp
-// Author      : Navid Saboori
-// Version     :
-// Copyright   : Sky2464
-// Description : N Queens in a N*N board in C++, Ansi-style
+// Name        : queen-n.cpp
+// Author      : Navid Saboori, Usman
+// Version     : 2
+// Professor   : Elaheh Vahdani
+// Copyright   : Your copyright notice
+// Description : Queen.cpp in C++, Ansi-style
 //============================================================================
-#include <iostream>
-#include <cstdlib>
-#include <cmath>
+
+/*
+ * PROGRAMMER : SUMIT JAIN
+ * OPERATING SYSTEM : MICROSOFT WINDOWS 10
+ * ENVIRONMENT : CODE::BLOCKS IDE
+ * PROGRAMMING LANGUAGE : GNU G++ 6.1.0 CYGWIN + MINGW
+ *
+ * NOTE : PLEASE READ THE COMMENTS CAREFULLY FOR BETTER UNDERSTANDING
+ */
+
+// PRE PROCESSOR DIRECTIVE
+#include <bits/stdc++.h>
+
+// NAMESPACE USED
 using namespace std;
 
-bool ok(int q[], int col)
-{
-for(int i=0;i<col;i++)
-{
-if(q[col]==q[i] || (col-i)==abs(q[col]-q[i])) return false;
-}
-return true;
+// WE ARE CONSIDERING nXn CHESS BOARD, THEREFORE, DEFINING N = user defined, but for the purpose of time and resource efficiency
+// for this project, user should manually change the define N 10 to some other number!
+#define N 12
+
+// PROTOTYPE DECLARATION
+bool isQueenSafe(char chessBoard[][N], int r, int c);
+void nQueen(char chessBoard[][N], int r, int c);
+
+// MAIN METHOD STARTS HERE
+
+int main() {
+
+// THIS ARRAY WILL KEEP TRACK OF THE POSITIONS OF QUEENS IN CURRENT CONFIGURATIONS
+	char chessBoard[N][N];
+
+// CHESS BOARD IS INITIALISED BY HYPHEN VALUE
+	memset(chessBoard, '0', sizeof chessBoard);
+
+// BLANK CHESS BOARD IS THEN PASSED TO THE nQueen PROBLEM
+	nQueen(chessBoard, 0, 0);
+
+	return 0;
 }
 
-bool backtrack(int &col)
-{
-    col--;
-    if(col==-1)
-    {
-    return false;
-    }
-    return true;
-}
+// THIS METHOD IS CALLED FROM THE MAIN METHOD
 
-void print(int q[],int m)
-{
-static int count=0;
-cout<<++count<<endl<<endl;
-int i;
-for(i=0;i<m;i++)
-{
-cout<<q[i]<<"  ";
-}
-cout<<endl<<endl;
-}
+void nQueen(char chessBoard[][N], int r, int c) {
+
+// TO PRINT ONLY 10 SOLUTIONS, TAKE A STATIC VARIABLE TO COUNT THROUGH 10
+	static int countSolutions = 0;
+
+// IF N QUEENS ARE IN CORRECT POSITION AS REQUIRED, THEN PRINT THE SOLUTION
+// IF USER WANT TO GET ORE SOLUTIONS, CHANGE 1 TO YOUR REQUIRED COUNT
+// IF REMOVE THE CONDITIONAL STATEMENT USER CAN ALL SOLUTIONS
 
 
-int main()
-{
-int n;
-    cout<<"enter the number of queens:"<<endl;
-    cin>>n;
-    for(int m=1;m<=n;m++)
-    {
-        int *q=new int [m];
-        q[0]=0;
-        int c=1;
-        bool from_backtrack=false;
-        int count=0;
-        bool a,b;
-        while(true)
-        {
-                a = true;
-                b = true;
-            while(c<m)
-            {
-                if(!from_backtrack)
-                {
-                    q[c]=-1;
-                }
-                from_backtrack=false;
-                while(q[c]<m)
-                {
-                    q[c]++;
-                    while(q[c]==m)
-                    {
-                        a = backtrack(c);
-                        if(a==false)
-                            break;
-                        q[c]++;
-                    }
-                    if(a==false)
-                        break;
-                    if(ok(q,c))
-                    {
-                        break;
-                    }
-                }
-                                if(a==false)
-                                    break;
-                c++;
-            }
-            bool b = backtrack(c);
-            if(a==false || b == false)
-                break;
-            count++;
-            from_backtrack=true;
-        }
-        delete []q;
-        cout<<m<<". There are "<<count<<" solutions to the "<<m<<" queens problem."<<endl;
-    }
-    return 0;
+	if (countSolutions < 1) {
+		if (c == 0 && r == N) {
+			countSolutions++;
+			cout << "\n\nSOLUTION NUMBER " << countSolutions << " :\n" << endl;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++)
+					cout << chessBoard[i][j] << " ";
+				cout << endl;
+			}
+			cout << endl;
+			return;
+		}
+	}
+
+// PLACE THE QUEEN AT EVERY SQUARE IN THE CURRENT ROW AND CHECK FOR VALID MOVE
+	for (int i = 0; i < N; i++) {
+
+// IF TWO CURRENT QUEENS ARE IN SAFE POSITIONS
+		if (isQueenSafe(chessBoard, r, i)) {
+
+// PLACE THE QUEEN ON THE CURRENT SQUARE ON CHESS BOARD
+			chessBoard[r][i] = '1';
+
+// RECURSIVELY CHECK FOR THE NEXT ROW
+			nQueen(chessBoard, r + 1, 0);
+
+// REMOVE QUEEN FROM QUEEN SQUARE AFTER BACKTRACKING
+			chessBoard[r][i] = '0';
+		}
+	}
 }
+
+// THIS METHOD WILL CHECK WHETHER TWO SUBSEQUENT QUEENS THREATENS EACH OTHER OR NOT
+
+bool isQueenSafe(char chessBoard[][N], int r, int c) {
+
+// THIS LOOP WILL RETURN FALSE WHEN TWO QUEENS SHARE THE SAME COLUMN
+	for (int i = 0; i < r; i++)
+		if (chessBoard[i][c] == '1')
+			return false;
+
+// THIS LOOP WILL RETURN FALSE WHEN TWO QUEENS SHARE THE SAME DIAGONAL BOXES (\)
+	for (int i = r, j = c; i >= 0 && j >= 0; i--, j--)
+		if (chessBoard[i][j] == '1')
+			return false;
+
+// THIS LOOP WILL RETURN FALSE WHEN TWO QUEENS SHARE THE SAME DIAGONAL BOXES (/)
+	for (int i = r, j = c; i >= 0 && j < N; i--, j++)
+		if (chessBoard[i][j] == '1')
+			return false;
+
+	return true; // OTHERWISE WILL RETURN TRUE
+}
+
+/*
+ * Test:
+
+FOR N == 10
+
+
+SOLUTION NUMBER 1 :
+
+1 0 0 0 0 0 0 0 0 0
+0 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 1 0 0 0 0
+0 0 0 0 0 0 0 1 0 0
+0 0 0 0 0 0 0 0 0 1
+0 0 0 0 1 0 0 0 0 0
+0 0 0 0 0 0 0 0 1 0
+0 1 0 0 0 0 0 0 0 0
+0 0 0 1 0 0 0 0 0 0
+0 0 0 0 0 0 1 0 0 0
+
+
+____________________
+
+n= 11;
+SOLUTION NUMBER 1 :
+
+1 0 0 0 0 0 0 0 0 0 0 0
+0 0 1 0 0 0 0 0 0 0 0 0
+0 0 0 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 1 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 0
+0 0 0 0 0 0 0 0 0 0 0 1
+0 0 0 0 0 1 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 1 0
+0 1 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 1 0 0 0 0 0
+0 0 0 0 0 0 0 0 1 0 0 0
+0 0 0 1 0 0 0 0 0 0 0 0
+
+
+
+		*/
